dea506b50257a728ca02416a6152c3562316e3f3
Kasper Grubbe <kawsper@gmail.com>
2021-11-12 01:13:20 +0100

    Set RAILS_ENV before we migrate and seed

--- benchmarks/railsbench/benchmark.rb
+++ benchmarks/railsbench/benchmark.rb
@@ -6,15 +6,15 @@ require 'harness'
 # and this app's db/seeds.rb will delete and repopulate
 # the database, so rows shouldn't accumulate.
 Dir.chdir __dir__
-use_gemfile extra_setup_cmd: "bin/rails db:migrate db:seed"
-
 ENV['RAILS_ENV'] ||= 'production'
+use_gemfile extra_setup_cmd: 'bin/rails db:migrate db:seed'
+
 require_relative 'config/environment'
 
 app = Rails.application
 
 possible_routes = ['/posts', '/posts.json']
-possible_routes.concat((1..100).map { |i| "/posts/#{i}"})
+possible_routes.concat((1..100).map { |i| "/posts/#{i}" })
 
 visit_count = 2000
 rng = Random.new(0x1be52551fc152997)
@@ -23,7 +23,7 @@ visiting_routes = Array.new(visit_count) { possible_routes.sample(random: rng) }
 run_benchmark(10) do
   visiting_routes.each do |path|
     # The app mutates `env`, so we need to create one every time.
-    env = Rack::MockRequest::env_for("http://localhost#{path}")
+    env = Rack::MockRequest.env_for("http://localhost#{path}")
     response_array = app.call(env)
     unless response_array.first == 200
       raise "HTTP response is #{response_array.first} instead of 200. Is the benchmark app properly set up? See README.md."
