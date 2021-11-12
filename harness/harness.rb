c459b3e9cfe58886e46a6d1547511b22a089571b
Kasper Grubbe <kawsper@gmail.com>
2021-11-12 01:12:43 +0100

    Set MIN_BENCH_ITRS to 42 (from 10)

--- harness/harness.rb
+++ harness/harness.rb
@@ -1,11 +1,11 @@
 require 'benchmark'
-require_relative "./harness-common"
+require_relative './harness-common'
 
 # Warmup iterations
 WARMUP_ITRS = ENV.fetch('WARMUP_ITRS', 15).to_i
 
 # Minimum number of benchmarking iterations
-MIN_BENCH_ITRS = ENV.fetch('MIN_BENCH_ITRS', 10).to_i
+MIN_BENCH_ITRS = ENV.fetch('MIN_BENCH_ITRS', 42).to_i
 
 # Minimum benchmarking time in seconds
 MIN_BENCH_TIME = ENV.fetch('MIN_BENCH_TIME', 10).to_i
@@ -16,13 +16,13 @@ OUT_CSV_PATH = File.expand_path(ENV.fetch('OUT_CSV_PATH', default_path))
 puts RUBY_DESCRIPTION
 
 # Takes a block as input
-def run_benchmark(num_itrs_hint)
+def run_benchmark(_num_itrs_hint, &block)
   times = []
   total_time = 0
   num_itrs = 0
 
   begin
-    time = Benchmark.realtime { yield }
+    time = Benchmark.realtime(&block)
     num_itrs += 1
 
     # NOTE: we may want to avoid this as it could trigger GC?
@@ -33,7 +33,7 @@ def run_benchmark(num_itrs_hint)
     # We internally save the time in seconds to avoid loss of precision
     times << time
     total_time += time
-  end until num_itrs >= WARMUP_ITRS + MIN_BENCH_ITRS and total_time >= MIN_BENCH_TIME
+  end until (num_itrs >= WARMUP_ITRS + MIN_BENCH_ITRS) && (total_time >= MIN_BENCH_TIME)
 
   # Write each time value on its own line
   File.write(OUT_CSV_PATH, "#{RUBY_DESCRIPTION}\n#{times.join("\n")}\n")
